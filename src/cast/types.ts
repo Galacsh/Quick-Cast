99a38ae5eecbd58bb9ed5b7141308367dbba0527
Galacsh <galacsh.dev@gmail.com>
2024-09-24 13:16:59 +0900

    fix: extract types to 'cast'

---
+++ src/cast/types.ts
@@ -0,0 +1,101 @@
+import type { ComponentType, ReactElement, ReactNode } from 'react'
+
+export type Keystroke = {
+  code: KeyboardEvent['code']
+  alt?: boolean
+  shift?: boolean
+  ctrlMeta?: boolean
+}
+
+// =============================
+
+export type ActionEssentials = {
+  title: string
+  shortcut?: Keystroke
+  onAction: () => void
+  isDefault?: boolean
+}
+
+export type ActionProps = ActionEssentials & {
+  icon?: ComponentType<{ className?: string }>
+}
+
+export type ActionPanelSectionProps = {
+  title: string
+  className?: string
+  children?: ActionElement | Iterable<ActionElement>
+}
+
+export type ActionPanelProps = {
+  className?: string
+  children?: ActionPanelChildren
+}
+
+export type ItemProps = {
+  id: string
+  icon?: ComponentType<{ className?: string }>
+  title: string
+  subtitle?: string
+  accessories?: ReactNode
+  actions: ActionPanelElement
+  className?: string
+}
+
+export type ItemSectionProps = {
+  title: string
+  className?: string
+  children?: ItemElement | Iterable<ItemElement>
+}
+
+export type ListProps = {
+  navigationTitle: string
+  searchBarPlaceholder: string
+  className?: string
+  children?:
+    | ItemElement
+    | Iterable<ItemElement>
+    | ItemSectionElement
+    | Iterable<ItemSectionElement>
+}
+
+export type ActionPanelElement = ReactElement<ActionPanelProps>
+
+export type ActionPanelSectionElement = ReactElement<ActionPanelSectionProps>
+
+export type ActionElement = ReactElement<ActionProps>
+
+export type ActionPanelChildren =
+  | ActionElement
+  | ActionPanelSectionElement
+  | Iterable<ActionElement | ActionPanelSectionElement>
+
+type ListElement = ReactElement<ListProps>
+
+type ItemSectionElement = ReactElement<ItemSectionProps>
+
+type ItemElement = ReactElement<ItemProps>
+
+// =============================
+
+export type Extension = {
+  id: string
+  name: string
+  icon: ComponentType<{ className?: string }>
+  commands: Command[]
+}
+
+export type Command = {
+  id: string
+  icon: ComponentType<{ className?: string }>
+  name: string
+} & (WithView | WithoutView)
+
+type WithView = {
+  mode: 'view'
+  view: ListElement
+}
+
+type WithoutView = {
+  mode: 'no-view'
+  action: () => void
+}
--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,1 @@
-import type { Dispatch, SetStateAction } from 'react'
-
 export type Theme = 'dark' | 'light' | 'system'
-export type ThemeProviderProps = {
-  children: React.ReactNode
-  initialTheme?: Theme
-}
-export type ThemeProviderState = {
-  theme: Theme
-  setTheme: Dispatch<SetStateAction<Theme>>
-}
-
-export type Keystroke = Pick<KeyboardEvent, 'code'> &
-  Partial<Pick<KeyboardEvent, 'altKey' | 'shiftKey'>> & {
-    ctrlMetaKey?: boolean
-  }
