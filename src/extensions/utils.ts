7246d7ae6534122c45e9cc1fab1b67eb7d99452f
Galacsh <galacsh.dev@gmail.com>
2024-09-27 12:36:55 +0900

    feat: util function to create requester and handlers

---
+++ src/extensions/utils.ts
@@ -0,0 +1,50 @@
+import type { Message, Response } from '@/types'
+
+export function payloadRequester<IN, OUT = void>(
+  id: string,
+  handler: (paylaod: IN) => Promise<OUT>
+) {
+  async function _requester(payload: IN) {
+    const message = { id, payload } as Message<IN>
+
+    const response = await chrome.runtime.sendMessage<
+      Message<IN>,
+      Response<OUT>
+    >(message)
+
+    return elseThrow(response) as OUT extends void ? void : OUT
+  }
+
+  return {
+    id,
+    handler,
+    requester: _requester,
+  }
+}
+
+export function requester<OUT = void>(id: string, handler: () => Promise<OUT>) {
+  async function _requester() {
+    const message = { id } as Message
+    const response = await chrome.runtime.sendMessage<Message, Response<OUT>>(
+      message
+    )
+
+    return elseThrow(response) as OUT extends void ? void : OUT
+  }
+
+  return {
+    id,
+    handler,
+    requester: _requester,
+  }
+}
+
+function elseThrow<OUT>(response: Response<OUT>) {
+  if (response.status === 'fail') {
+    throw new Error(response.error)
+  }
+
+  if ('data' in response) {
+    return response.data
+  }
+}
