48fa3be19f029d77d097876d3863770494fba9e8
Galacsh <galacsh.dev@gmail.com>
2024-09-27 12:34:02 +0900

    feat: request-response between popup and content script

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,4 +1,6 @@
 import { name, version } from '@/../package.json'
+import { handlers } from '@/extensions'
+import type { MessageSender, Message, Handler, SendResponse } from '@/types'
 
 function openOptionsPage() {
   chrome.runtime.openOptionsPage()
@@ -31,6 +33,49 @@ function onInstallOrUpdate(detail: chrome.runtime.InstalledDetails) {
   }
 }
 
+function onAction(
+  message: Message<unknown>,
+  _: MessageSender,
+  sendResponse: SendResponse
+) {
+  type HandlerId = keyof typeof handlers
+  const id = message.id as HandlerId
+
+  const handler = handlers[id]
+
+  try {
+    if ('payload' in message) {
+      // treat handler as a payload handler
+      type PayloadHandler = Handler<(p: unknown) => Promise<unknown>>
+      const withPayload = handler as PayloadHandler
+
+      // run handler and send response
+      withPayload(message.payload).then((handled) => {
+        if (handled != null) sendResponse({ status: 'ok', data: handled })
+        else sendResponse({ status: 'ok' })
+      })
+    } else {
+      // treat handler as a handler without payload
+      type NoPayloadHandler = Handler<(p: void) => Promise<unknown>>
+      const withoutPayload = handler as NoPayloadHandler
+
+      // run handler and send response
+      withoutPayload().then((handled) => {
+        if (handled != null) sendResponse({ status: 'ok', data: handled })
+        else sendResponse({ status: 'ok' })
+      })
+    }
+  } catch (e) {
+    sendResponse({
+      status: 'fail',
+      error: (e as Error).message,
+    })
+  }
+
+  return true
+}
+
 // ======================================================
 
 chrome.runtime.onInstalled.addListener(onInstallOrUpdate)
+chrome.runtime.onMessage.addListener(onAction)
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,31 @@
 export type Theme = 'dark' | 'light' | 'system'
+
+// ================================
+
+export type Message<P = void> = P extends void
+  ? { id: string }
+  : { id: string; payload: P }
+
+export type ResponseFail = { status: 'fail'; error: string }
+export type ResponseOkay<T = void> = T extends void
+  ? { status: 'ok' }
+  : { status: 'ok'; data: T }
+export type Response<T = void> = ResponseFail | ResponseOkay<T>
+
+export type HasNoPayload<T> = T extends () => infer OUT ? () => OUT : never
+export type HasPayload<T> = T extends (payload: infer IN) => infer OUT
+  ? (payload: IN) => OUT
+  : never
+export type Handler<T> =
+  HasNoPayload<T> extends never ? HasPayload<T> : HasNoPayload<T>
+
+// ================================
+
+// chrome type aliases
+export type Tab = chrome.tabs.Tab
+export type TabGroup = chrome.tabGroups.TabGroup
+export type BookmarkNode = chrome.bookmarks.BookmarkTreeNode
+export type BookmarkRemoveInfo = chrome.bookmarks.BookmarkRemoveInfo
+export type MessageSender = chrome.runtime.MessageSender
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type SendResponse = (response?: any) => void
