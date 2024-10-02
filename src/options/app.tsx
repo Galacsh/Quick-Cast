c3b540488c7947163628de078334dd78d65fd608
Galacsh <galacsh.dev@gmail.com>
2024-10-02 14:58:47 +0900

    feat: better options page

--- src/options/app.tsx
+++ src/options/app.tsx
@@ -1,10 +1,14 @@
 import { ThemeProvider } from '@/components/theme-provider'
 import ThemeOption from './theme'
+import { cn } from '@/lib/utils'
 
 export default function App() {
   return (
     <ThemeProvider>
-      <ThemeOption />
+      <div className={cn('container py-4', 'flex flex-col gap-4')}>
+        <h1 className={cn('font-extrabold text-xl')}>Quick Cast</h1>
+        <ThemeOption />
+      </div>
     </ThemeProvider>
   )
 }
--- src/options/theme.tsx
+++ src/options/theme.tsx
@@ -1,13 +1,23 @@
 import { Button } from '@/components/ui/button'
 import { useTheme } from '@/lib/hooks/use-theme'
+import { cn } from '@/lib/utils'
 
 export default function ThemeOption() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
   return (
-    <div>
-      <Button onClick={() => setTheme('light')}>Light</Button>
-      <Button onClick={() => setTheme('dark')}>Dark</Button>
-      <Button onClick={() => setTheme('system')}>System</Button>
+    <div className={cn('flex flex-col gap-3')}>
+      <div>
+        <h2 className="font-bold text-lg">Theme Options</h2>
+      </div>
+      <div className="text-sm text-foreground/70">
+        <span className="mr-2">Current theme:</span>
+        <span>{theme.charAt(0).toUpperCase() + theme.slice(1)}</span>
+      </div>
+      <div className="flex items-center gap-3">
+        <Button onClick={() => setTheme('light')}>Light</Button>
+        <Button onClick={() => setTheme('dark')}>Dark</Button>
+        <Button onClick={() => setTheme('system')}>System</Button>
+      </div>
     </div>
   )
 }
